4982c3d32ffa9dff1496a4c76b3f96dc6b5ca046
cashewnut <xiyaoguo@yeah.net>
2019-06-17 16:02:01 +0800

    modify

--- src/main/java/C.java
+++ src/main/java/C.java
@@ -1,27 +1,10 @@
-import com.sun.org.apache.xerces.internal.impl.xs.opti.AttrImpl;
-import com.sun.org.apache.xml.internal.security.signature.XMLSignatureInput;
 import com.sun.org.apache.xml.internal.security.utils.resolver.ResourceResolverException;
-import com.sun.org.apache.xml.internal.security.utils.resolver.ResourceResolverSpi;
-import com.sun.org.apache.xml.internal.security.utils.resolver.implementations.ResolverAnonymous;
-import com.sun.xml.internal.messaging.saaj.soap.name.NameImpl;
-import org.w3c.dom.Attr;
-import sun.net.www.protocol.ftp.Handler;
 
-import javax.management.monitor.CounterMonitor;
 import javax.swing.*;
 import javax.swing.text.BoxView;
 import javax.swing.text.View;
-import javax.xml.soap.Name;
-import javax.xml.soap.SOAPElement;
-import javax.xml.soap.SOAPElementFactory;
 import javax.xml.soap.SOAPException;
-import javax.xml.stream.XMLInputFactory;
-import java.awt.*;
-import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URLStreamHandler;
-import java.rmi.server.RMIClassLoader;
 import java.util.Date;
 
 public class C {
